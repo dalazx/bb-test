558688125aa4a3b0d79c647a517834b623fd8c72
Aleksandr Danshyn <dalazx@gmail.com>
2015-08-27 17:03:21 +0300

    Pointed the engine to an in-memory sqlite db

--- bb_test/db.py
+++ bb_test/db.py
@@ -5,13 +5,15 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 
 
-engine = create_engine('mysql://root@localhost/bb_test', echo=True)
+# engine = create_engine('mysql://root@localhost/bb_test', echo=True)
+engine = create_engine('sqlite:///:memory:', echo=True)
 Session = scoped_session(sessionmaker(bind=engine))
 Base = declarative_base(bind=engine)
 
 
 class User(Base):
     __tablename__ = 'dedsert_test'
+    __table_args__ = {'mysql_engine': 'InnoDB'}
 
     id = Column(Integer, primary_key=True)
     name = Column(String(64))
